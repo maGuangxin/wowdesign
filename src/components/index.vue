1c6c5ebb0f57093f259db4f9fbcec6eab604368e
maGuangxin <363991425@qq.com>
2018-10-16 17:08:11 +0800

    尖叫设计复刻-v1.0.1

---
+++ src/components/index.vue
@@ -0,0 +1,35 @@
+<template>
+ 	<div>
+ 		{{name}}
+		<adbar></adbar>
+		<searchbar></searchbar>
+		<tagbox></tagbox>
+		<footer></footer>	
+ 	</div>
+</template>
+
+<script>
+	import adbar from './index/adbar'
+	import searchbar from './index/searchbar'
+	import tagbox from './index/tagbox'
+	import footerbar from './index/footerbar'
+export default {
+  	name:'index',
+  	data(){
+  		return {
+  			name:'首页'
+  		}
+  	},
+  	components:{
+  		adbar,
+  		searchbar,
+  		tagbox,
+  		footerbar
+  	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/index/footerbar.vue
@@ -0,0 +1,20 @@
+<template>
+ 	<div>{{name}}</div>
+ 	
+</template>
+
+<script>
+export default {
+  	name:'adBar',
+  	data(){
+  		return {
+  			name:'足部导航'
+  		}
+  	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
